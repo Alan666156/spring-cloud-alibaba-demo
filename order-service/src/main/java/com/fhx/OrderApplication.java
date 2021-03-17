661a96b8d09208094003130f2eb4dd9b83374d16
alan <alan666156@163.com>
2021-03-17 23:47:04 +0800

    init

--- order-service/src/main/java/com/fhx/OrderApplication.java
+++ order-service/src/main/java/com/fhx/OrderApplication.java
@@ -1,9 +1,12 @@
 package com.fhx;
 
+import cn.hutool.json.JSONString;
+import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  * Hello world!
@@ -16,7 +19,7 @@ public class OrderApplication
 {
     public static void main( String[] args )
     {
-        SpringApplication.run(OrderApplication.class, args);
-        log.info("===============Stock START SUCCESS==============");
+        ConfigurableApplicationContext configurableApplicationContext = SpringApplication.run(OrderApplication.class, args);
+        log.info("===============Order START SUCCESS==============");
     }
 }
